1fa0ffdffd3110d0b56c01a9346e6262a7bb6df6
robosek <rose@powel.com>
2018-05-19 22:07:25 +0200

    Use win-64 build target, use original data and move trainer to spearate module

--- SpamOrHam/NaiveBayes.fs
+++ SpamOrHam/NaiveBayes.fs
@@ -1,29 +1,48 @@
-namespace NaiveBayes
+(* #r @"bin/Debug/net461/win-x64/Microsoft.ML.dll"
+#r @"bin/Debug/net461/win-x64/Microsoft.ML.Api.dll"
+#r @"bin/Debug/net461/win-x64/Microsoft.ML.Core.dll"
+#r @"bin/Debug/net461/win-x64/Microsoft.ML.Data.dll"
+#r @"bin/Debug/net461/win-x64/Microsoft.ML.CpuMath.dll"
+#r @"bin/Debug/net461/win-x64/netstandard.dll"
+#r @"bin/Debug/net461/win-x64/CpuMathNative.dll"
+ *)
 
-module Classifier = 
-    type Token = string
+module Training 
+    open Microsoft.ML
+    open Microsoft.ML.Runtime.Api;
+    open Microsoft.ML.Trainers;
+    open Microsoft.ML.Transforms;
+    open Microsoft.ML.Runtime.Data
 
-    type Tokenizer = string -> Token Set
+    type SpamOrHam() =
+        [<Column(ordinal = "0", name="Label"); DefaultValue>]
+        val mutable Label: string 
+        [<Column(ordinal = "1"); DefaultValue>]
+        val mutable Content: string
 
-    type TokenizedDoc = Token Set
+    type CategoryPrediction() = 
+        [<ColumnName "PredictedLabel"; DefaultValue>]
+        val mutable PredictedLabel: string
 
-    type DocsGroup = {
-        Proportion: float
-        TokenFrequencies: Map<Token, float>
-    }
+    let trainAndPredictData dataFileName = 
+        let learningPipeline = new LearningPipeline()
+        let textLoader = new TextLoader<SpamOrHam>(dataFileName, false, "tab")
+        let dictionizer = new Dictionarizer("Label")
+        let textFeaturizer = new TextFeaturizer("Features", "Content")
+        let predictLabelConverter = new PredictedLabelColumnOriginalValueConverter()
+        predictLabelConverter.PredictedLabelColumn <- "PredictedLabel"
+        let classifier = new FastTreeBinaryClassifier()
+        classifier.NumLeaves <- 5
+        classifier.NumTrees <- 5
+        classifier.MinDocumentsInLeafs <- 2
 
-    let tokenScore (docsGroup: DocsGroup) (token: Token) =
-        if docsGroup.TokenFrequencies.ContainsKey token then
-            log docsGroup.TokenFrequencies.[token]
-        else
-            0.0
-    
-    let score (document: TokenizedDoc) (docsGroup: DocsGroup) =
-        let scoreToken = tokenScore docsGroup
-        log docsGroup.Proportion + (document |> Seq.sumBy scoreToken)
-    
-    let classify (groups: (_*DocsGroup)[]) (tokenizer: Tokenizer) (text: string) = 
-        let tokenized = tokenizer text
-        groups
-        |> Array.maxBy(fun (_,group) -> score tokenized group)
-        |> fst
+        learningPipeline.Add(textLoader) |> ignore
+        learningPipeline.Add dictionizer
+        learningPipeline.Add(textFeaturizer) |> ignore
+        learningPipeline.Add(classifier) |> ignore
+        learningPipeline.Add(predictLabelConverter) |> ignore
+
+        let model = learningPipeline.Train<SpamOrHam, CategoryPrediction>()        
+        let test = new SpamOrHam()
+        test.Content <- "REMINDER FROM O2: To get 2.50 pounds free call credit and details of great offers pls reply 2 this text with your valid name, house no and postcode"
+        model.Predict(test)
