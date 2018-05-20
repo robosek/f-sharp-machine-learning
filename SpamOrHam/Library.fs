4a69f02811480def45f81e2647ca054204f7f8cb
Robert Sęk <robosek@tlen.pl>
2018-05-20 21:36:09 +0200

    Add falsePositive and falseNegative parameters

--- SpamOrHam/Library.fs
+++ SpamOrHam/Library.fs
@@ -3,8 +3,12 @@ let main argv =
     let model = Training.trainData "data/train_data.txt"
     let metrics = Training.evaluateData model "data/evaluate_data.txt"
     let customAccuracy = Training.customEvaluator model "data/evaluate_data.txt"
-    
+    let falsePositive = Training.getDisambiguation model "data/evaluate_data.txt" "ham"
+    let falseNegative = Training.getDisambiguation model "data/evaluate_data.txt" "spam"
+
     printfn "Micro accuracy is : %f" metrics.AccuracyMicro
     printfn "Macro accuracy is : %f" metrics.AccuracyMacro
     printfn "Custom accuracy is : %f" customAccuracy
+    printfn "False positive average is: %f" falsePositive
+    printfn "False negative average is: %f" falseNegative
     0
